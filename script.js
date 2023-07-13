186117f307a1eaba6f8aa00cc21251765a0144d7
Buzzoka <131547251+Buzzoka@users.noreply.github.com>
2023-07-13 02:23:21 +0000

    aa

--- script.js
+++ script.js
@@ -7,29 +7,58 @@ const SCOPES = 'user-read-currently-playing';
 // Construct the authorization URL with the scope
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(SCOPES)}&response_type=token`;
 
-// Function to fetch the currently playing song from the server
+// Authorization token endpoint
+const AUTH_TOKEN_URL = 'https://accounts.spotify.com/api/token';
+
+// Spotify API base URL
+const API_BASE_URL = 'https://api.spotify.com/v1';
+
+// Access token variable
+let accessToken = '';
+
+// Function to retrieve access token
+async function getAccessToken() {
+  // Use the access token from the URL fragment if available
+  const params = new URLSearchParams(window.location.hash.substr(1));
+  accessToken = params.get('access_token');
+
+  // If access token is present, use it
+  if (accessToken) {
+    return accessToken;
+  }
+
+  // If access token is not available, redirect user to authorization URL
+  window.location.href = AUTH_URL;
+}
+
+// Function to fetch the currently playing song
 async function fetchCurrentlyPlayingSong() {
+  const currentlyPlayingUrl = `${API_BASE_URL}/me/player/currently-playing`;
+
   try {
-    const response = await fetch('http://localhost:3000/currently-playing');
+    const response = await fetch(currentlyPlayingUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
     if (response.ok) {
       const data = await response.json();
-      if (data.songTitle) {
-        const songTitle = data.songTitle;
-        const artistName = data.artistName;
-        const songDuration = data.songDuration;
-        const progressMs = data.progressMs;
+      if (data.item) {
+        const songTitle = data.item.name;
+        const artistName = data.item.artists[0].name;
+        const songDuration = data.item.duration_ms / 1000; // Convert duration from milliseconds to seconds
+        const progressMs = data.progress_ms; // Current playback position in milliseconds
 
         // Update the widget with the currently playing song information
         document.getElementById('info_title').textContent = songTitle;
         document.getElementById('info_artist').textContent = artistName;
         document.getElementById('time-total').textContent = formatTime(songDuration);
-        document.getElementById('time-elapsed').textContent = formatTime(progressMs / 1000);
-
-        const progressPercentage = (progressMs / songDuration) * 100;
-        document.getElementById('progress_top').style.width = `${progressPercentage}%`;
+        document.getElementById('time-elapsed').textContent = formatTime(progressMs / 1000); // Convert progress from milliseconds to seconds
 
         // Update the progress bar
-        updateProgressBar(progressMs / 1000, songDuration);
+        const progressPercentage = (progressMs / songDuration) * 100;
+        document.getElementById('progress_top').style.width = `${progressPercentage}%`;
       } else {
         // No currently playing song
         document.getElementById('info_title').textContent = 'Nothing is playing';
@@ -47,9 +76,14 @@ async function fetchCurrentlyPlayingSong() {
 }
 
 // Function to update the progress bar
-function updateProgressBar(currentTime, duration) {
-  const progressPercentage = (currentTime / duration) * 100;
-  document.getElementById('progress_top').style.width = `${progressPercentage}%`;
+function updateProgressBar() {
+  // Get the current playback position and duration from your music player
+  // Replace the hardcoded values below with your actual implementation
+  const currentTime = 125; // Example: replace with actual current playback position in seconds
+  const duration = 243; // Example: replace with actual song duration in seconds
+
+  // Update the progress bar
+  document.getElementById('progress_top').style.width = `${(currentTime / duration) * 100}%`;
   document.getElementById('time-elapsed').textContent = formatTime(currentTime);
   document.getElementById('time-total').textContent = formatTime(duration);
 }
@@ -57,9 +91,13 @@ function updateProgressBar(currentTime, duration) {
 // Helper function to format time as MM:SS
 function formatTime(time) {
   const minutes = Math.floor(time / 60);
-  const seconds = Math.floor(time % 60);
+  const seconds = time % 60;
+
   return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
 // Call the necessary functions
-fetchCurrentlyPlayingSong();
+getAccessToken().then(() => {
+  fetchCurrentlyPlayingSong();
+  updateProgressBar();
+});
